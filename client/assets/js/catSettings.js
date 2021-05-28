3b350d43f89db90a90e7c4ea6681bd7f79c94683
Xyz Fiegalan <xyzfiegalan@gmail.com>
2021-05-29 03:09:01 +0800

    frontend done

--- client/assets/js/catSettings.js
+++ client/assets/js/catSettings.js
@@ -2,17 +2,17 @@
 var colors = Object.values(allColors())
 
 var defaultDNA = {
-    "headcolor" : 21,
-    "mouthColor" : 39,
-    "eyesColor" : 16,
-    "earsColor" : 42,
-    //Cattributes
-    "eyesShape" : 1,
-    "decorationPattern" : 1,
-    "decorationMidcolor" : 61,
-    "decorationSidescolor" : 61,
-    "animation" :  1,
-    "lastNum" :  1
+  "headcolor" : 96,
+  "mouthColor" : 39,
+  "eyesColor" : 98,
+  "earsColor" : 98,
+  //Cattributes
+  "eyesShape" : 6,
+  "decorationPattern" : 3,
+  "decorationMidcolor" : 84,
+  "decorationSidescolor" : 69,
+  "animation" :  6,
+  "lastNum" :  1
 }
 
 // when page load
@@ -33,59 +33,59 @@ $( document ).ready(function() {
 });
 
 function getDna(){
-    var dna = ''
-    dna += $('#dnabody').html()
-    dna += $('#dnamouth').html()
-    dna += $('#dnaeyes').html()
-    dna += $('#dnaears').html()
-    dna += $('#dnashape').html()
-    dna += $('#dnadecoration').html()
-    dna += $('#dnadecorationMid').html()
-    dna += $('#dnadecorationSides').html()
-    dna += $('#dnaanimation').html()
-    dna += $('#dnaspecial').html()
+  var dna = ''
+  dna += $('#dnabody').html()
+  dna += $('#dnamouth').html()
+  dna += $('#dnaeyes').html()
+  dna += $('#dnaears').html()
+  dna += $('#dnashape').html()
+  dna += $('#dnadecoration').html()
+  dna += $('#dnadecorationMid').html()
+  dna += $('#dnadecorationSides').html()
+  dna += $('#dnaanimation').html()
+  dna += $('#dnaspecial').html()
 
-    return parseInt(dna)
+  return parseInt(dna)
 }
 
 function renderCat(dna){
-    headColor(colors[dna.headcolor],dna.headcolor)
-    $('#bodycolor').val(dna.headcolor) //sets slider value
+  headColor(colors[dna.headcolor],dna.headcolor)
+  $('#bodycolor').val(dna.headcolor) //sets slider value
 
-    mouthColor(colors[dna.mouthColor],dna.mouthColor)
-    $('#bodycolor').val(dna.mouthColor)
+  mouthColor(colors[dna.mouthColor],dna.mouthColor)
+  $('#bodycolor').val(dna.mouthColor)
 
-    eyeColor(colors[dna.eyesColor],dna.eyesColor)
-    $('#eyescolor').val(dna.eyesColor)
+  eyeColor(colors[dna.eyesColor],dna.eyesColor)
+  $('#eyescolor').val(dna.eyesColor)
 
-    earColor(colors[dna.earsColor],dna.earsColor)
-    $('#earscolor').val(dna.earsColor)
+  earColor(colors[dna.earsColor],dna.earsColor)
+  $('#earscolor').val(dna.earsColor)
 
-    eyeVariation(dna.eyesShape)
-    $('#eyeshape').val(dna.eyesShape)
+  eyeVariation(dna.eyesShape)
+  $('#eyeshape').val(dna.eyesShape)
 
-    hairStyle(dna.decorationPattern)
-    $('#hairstyle').val(dna.decorationPattern)
+  hairStyle(dna.decorationPattern)
+  $('#hairstyle').val(dna.decorationPattern)
 
-    middleHairColor(colors[dna.decorationMidcolor],dna.decorationMidcolor)
-    $('#hairColor1').val(dna.decorationMidcolor)
+  middleHairColor(colors[dna.decorationMidcolor],dna.decorationMidcolor)
+  $('#hairColor1').val(dna.decorationMidcolor)
 
-    sidesHairColor(colors[dna.decorationSidescolor],dna.decorationSidescolor)
-    $('#hairColor2').val(dna.decorationSidescolor)
+  sidesHairColor(colors[dna.decorationSidescolor],dna.decorationSidescolor)
+  $('#hairColor2').val(dna.decorationSidescolor)
 
-    animationVariation(dna.animation)
-    $('#animation').val(dna.animation)
+  animationVariation(dna.animation)
+  $('#animation').val(dna.animation)
 }
 
 // Changing cat colors
 $('#bodycolor').change(()=>{
-    var colorVal = $('#bodycolor').val()
-    headColor(colors[colorVal],colorVal)
+  var colorVal = $('#bodycolor').val()
+  headColor(colors[colorVal],colorVal)
 })
 
 $('#mouthcolor').change( () => {
-    var colorVal = $('#mouthcolor').val()
-    mouthColor(colors[colorVal], colorVal)
+  var colorVal = $('#mouthcolor').val()
+  mouthColor(colors[colorVal], colorVal)
 })
 
 $('#eyescolor').change( () => {
@@ -123,4 +123,48 @@ $('#hairstyle').change( () => {
 $('#animation').change( () => {
   var animationVal = parseInt($('#animation').val())
   animationVariation(animationVal)
+})
+
+//Random Kitty
+$('#random').click(() => {
+  var bodyColorRandom = Math.floor(Math.random() * 88) + 10;
+  headColor(colors[bodyColorRandom],bodyColorRandom)
+  $("#bodycolor").val(bodyColorRandom)
+
+  var mouthColorRandom = Math.floor(Math.random() * 88) + 10;
+  mouthColor(colors[mouthColorRandom],mouthColorRandom)
+  $("#mouthcolor").val(mouthColorRandom)
+
+  var eyesColorRandom = Math.floor(Math.random() * 88) + 10;
+  eyeColor(colors[eyesColorRandom],eyesColorRandom)
+  $("#eyescolor").val(eyesColorRandom)
+
+  var earsColorRandom = Math.floor(Math.random() * 88) + 10;
+  earColor(colors[earsColorRandom],earsColorRandom)
+  $("#earscolor").val(earsColorRandom)
+
+  var eyeShapeRandom = Math.floor(Math.random() * 7) + 1;
+  eyeVariation(eyeShapeRandom)
+  $("#eyeshape").val(eyeShapeRandom)
+
+  var hairStyleRandom = Math.floor(Math.random() * 7) + 1;
+  hairStyle(hairStyleRandom)
+  $("#hairstyle").val(hairStyleRandom)
+
+  var hairMidColor = Math.floor(Math.random() * 88) + 10;
+  middleHairColor(colors[hairMidColor],hairMidColor)
+  $("#hairColor1").val(hairMidColor)
+
+  var hairSideColor = Math.floor(Math.random() * 88) + 10;
+  sidesHairColor(colors[hairSideColor],hairSideColor)
+  $("#hairColor2").val(hairSideColor)
+
+  var randomAnimation = Math.floor(Math.random() * 6) + 1;
+  animationVariation(randomAnimation)
+  $("#animation").val(randomAnimation)
+})
+
+//Default Kitty
+$('#default').click(() => {
+  renderCat(defaultDNA)  
 })
