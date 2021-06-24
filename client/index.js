f7f7a7c508042cdce9c013e03a88088a7c297015
Xyz Fiegalan <xyzfiegalan@gmail.com>
2021-06-24 09:54:17 +0800

    create kitty button

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ var web3 = new Web3(Web3.givenProvider);
 
 var instance;
 var user;
-var contractAddress = "0x61b369574c6249C14983cd44e14EfE5137EAe1ba"; //enter contact address here
+var contractAddress = "0x9C06fA8d1cA9C3652b14E3A3e0f86B5921e3fB1C"; //enter contact address here
 
 $(document).ready(function(){
     window.ethereum.enable().then(function(accounts){
@@ -10,5 +10,39 @@ $(document).ready(function(){
         user = accounts[0];
 
         console.log(instance);
+        
+        instance.events.Birth().on('connected', function(subscriptionId) {
+            console.log(subscriptionId);
+        }) 
+        .on('error', function(error){
+            console.log(error);
+            alert("Kitty Birth failed");
+        })
+        .on('data', function(event) {
+            alert(`Kitty birth successful! \n
+            Kitty Owner: ${event.returnValues.owner}\n
+            Kitty ID: ${event.returnValues.kittenId}\n
+            Mommy Cat: ${event.returnValues.mumId}\n
+            Daddy Cat: ${event.returnValues.dadId}\n
+            Kitty Genes: ${event.returnValues.genes}
+            `)
+        })
     })
 })
+
+function createKitty() {
+    var kittyDna = getDna();
+    instance.methods.createKittyGen0(kittyDna).send({}, function(error, txHash) {
+        if(error) 
+            console.log(err);
+        else 
+            console.log(txHash); 
+    })
+}
+
+$('#create').click(() => {
+    createKitty(getDna());
+})
+
+
+
